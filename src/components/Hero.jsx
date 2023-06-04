561ca52f9a8c323299ead1c5245021e7381a3f41
prrockzed <prrockzed@pm.me>
2023-06-04 14:23:25 +0530

    Fixed the scrolling mouse for small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center hidden sm:flex'>
         <a href='#about'>
           <div className='w-[25px] h-[44px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2'>
             <motion.div
