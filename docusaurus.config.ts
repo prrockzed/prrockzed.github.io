1379e0c64df846248a9c63ef8f53c24281e50a63
prrockzed <priyanshuranjan2806@gmail.com>
2024-11-13 23:03:09 +0530

    Initiated Yarn and added gh-pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'prrockzed', // Usually your GitHub org/user name.
   projectName: 'prrockzed.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // Usually gh-pages branch.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
